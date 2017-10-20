7b79ada775bff0f4de081c25a2900a60b3e2a152
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-20 19:39:59 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,8 +2,8 @@ class Puppy
   attr_accessor :name, :breed, :age
 
 
-  def initialize(name, age, breed)
-    @name, @age, @breed = name, age, breed
+  def initialize(name, breed, age)
+    @name, @breed, @age = name, breed, age
   end
 
 
