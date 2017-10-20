f71ee8d413ce8a97319fa4b600c09ddbd0097a54
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-20 19:56:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,15 +7,18 @@ class App < Sinatra::Base
     erb :index
   end
 
-  # Get request to /NEW which has the create_puppy.erb file & form
+  # Get request to /new which has the create_puppy.erb file & form
   get '/new' do
     erb :create_puppy
   end
 
   # Take user input to POST to display_puppy.erb file/page
   post '/' do
-    @puppy = Puppy.new(@name, @breed, @age)
-    
+    # Assign input parameters to instance variables
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+
     erb :display_puppy
   end
 
