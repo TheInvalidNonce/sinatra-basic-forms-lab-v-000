3b65f8d60dcadf20ee6fd4b251aabe401f027c8f
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-20 19:44:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  # Render the index page with the link to the puppy form
   get '/' do
     erb :index
   end
 
+  get '/NEW' do
+    erb :create_puppy
+  end
   # post ''
 
 end
